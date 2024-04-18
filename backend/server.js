e485592c7ede87f52051ad9371ea80e8d63ad69b
gabriel geronimo <bielgeronimo10@gmail.com>
2024-04-18 01:27:32 -0300

    [feat] criacao das rotas de envio e obter mensagens

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5001;
 dotenv.config();
 
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to parse incoming requests with cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
